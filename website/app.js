517f4f759a5a9035d09df2ccc2620a3d52192e42
Orne Virardi <73016255+ornevirardi@users.noreply.github.com>
2021-12-30 15:41:36 -0800

    Update app.js

--- website/app.js
+++ website/app.js
@@ -8,71 +8,79 @@ const zipCode = "94112";
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
+//Event listener to actually get stuff done + update the UI using then to chain:
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e){
+  const zipData =  document.getElementById('zip').value;
+  const feelings =  document.getElementById('feelings').value;
+    postData("/weatherData", {
+    temperature: 123,
+    date: new Date(),
+    userResponse: "hello"
+  }).then(() => updateUI());
+}
 //getData and postData from the lesson 3, concept 6: Client side, Server side & "putting it all together - exercise":
 
 const postData = async (url = "", data = {}) => {
-  console.log(data);
-  const response = await fetch(url, {
-    method: "POST",
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  });
+    console.log(data);
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+  
+    try {
+      const newData = await response.json();
+      console.log(newData);
+      return newData;
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
-  try {
-    const newData = await response.json();
-    console.log(newData);
-    return newData;
-  } catch (error) {
-    console.log("error", error);
-  }
-};
-postData("/weatherData", {
-  temperature: 123,
-  date: new Date(),
-  userResponse: "hello"
-}).then(() => getData());
+  const getData = async function() {
+    const request = await fetch("/all");
+    try {
+      const all = await request.json();
+      console.log(
+        "getData response: " +
+          all.temperature +
+          "|| " +
+          all.date +
+          "|| " +
+          all.userResponse
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  const getDataOpenWeather = async (apiUrl, apiKey, zipCode) => {
+    const request = await fetch(apiUrl + apiKey + zipCode);
+    try {
+      const allData = await request.json();
+      console.log(allData);
+      return allData;
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
-const getData = async function() {
-  const request = await fetch("/all");
-  try {
-    const all = await request.json();
-    console.log(
-      "getData response: " +
-        all.temperature +
-        "|| " +
-        all.date +
-        "|| " +
-        all.userResponse
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const getDataOpenWeather = async (apiUrl, apiKey, zipCode) => {
-  const request = await fetch(apiUrl + apiKey + zipCode);
-  try {
-    const allData = await request.json();
-    console.log(allData);
-    return allData;
-  } catch (error) {
-    console.log("error", error);
-  }
-};
-
-//update UI - Lesson 4- concept 10
-
-const updateUI = async () => {
-  const request = await fetch("/all");
-  try {
-    const allData = await request.json();
-    document.getElementById("date").innerHTML = allData[0].date;
-    document.getElementById("temp").innerHTML = allData[0].temperature;
-    document.getElementById("content").innerHTML = allData[0].userResponse;
-  } catch (error) {
-    console.log("error", error);
-  }
-}
+  //update UI - Lesson 4- concept 10:
+  
+  const updateUI = async () => {
+    const request = await fetch("/all");
+    try {
+      const allData = await request.json();
+      document.getElementById("date").innerHTML = allData.date;
+      document.getElementById("temp").innerHTML = allData.temperature;
+      document.getElementById("content").innerHTML = allData.userResponse;
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
