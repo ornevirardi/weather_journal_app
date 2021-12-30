cfadfa0d11223584bf1cf51d1ba541c34ef9ef72
Orne Virardi <73016255+ornevirardi@users.noreply.github.com>
2021-12-30 14:39:11 -0800

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,40 +1,78 @@
 /* Global Variables */
 
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "c336cc1b4bc149e2a2a250056871f723";
+const zipCode = "94112";
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
-//postData from the lesson 3, concept 6: Client side, Server side:
+//getData and postData from the lesson 3, concept 6: Client side, Server side & "putting it all together - exercise":
 
-const postData = async ( url = '', data = {})=>{
-    console.log(data);
-      const response = await fetch(url, {
-      method: 'POST', 
-      credentials: 'same-origin',
-      headers: {
-          'Content-Type': 'application/json',
-      },       
-      body: JSON.stringify(data), 
-    });
+const postData = async (url = "", data = {}) => {
+  console.log(data);
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
 
-      try {
-        const newData = await response.json();
-        console.log(newData);
-        return newData;
-      }catch(error) {
-      console.log("error", error);
-      }
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
   }
+};
+postData("/weatherData", {
+  temperature: 123,
+  date: new Date(),
+  userResponse: "hello"
+}).then(() => getData());
 
+const getData = async function() {
+  const request = await fetch("/all");
+  try {
+    const all = await request.json();
+    console.log(
+      "getData response: " +
+        all.temperature +
+        "|| " +
+        all.date +
+        "|| " +
+        all.userResponse
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  const getData = async function () {
-    const request = await fetch("/all");
-    try {
-      const all = await request.json();   
-      console.log("getData response: "+  all.temperature + "|| " +  all.date + "|| " +  all.userResponse);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  postData("/weatherData", {temperature:123, date:new Date(), userResponse:"hello"})
-    .then( () => getData())
+const getDataOpenWeather = async (apiUrl, apiKey, zipCode) => {
+  const request = await fetch(apiUrl + apiKey + zipCode);
+  try {
+    const allData = await request.json();
+    console.log(allData);
+    return allData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+//update UI - Lesson 4- concept 10
+
+const updateUI = async () => {
+  const request = await fetch("/all");
+  try {
+    const allData = await request.json();
+    document.getElementById("date").innerHTML = allData[0].date;
+    document.getElementById("temp").innerHTML = allData[0].temperature;
+    document.getElementById("content").innerHTML = allData[0].userResponse;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
